80e1c3c538be58ed5cd1fd16426e142a4ca69f81
wiidede <wiixdede@gmail.com>
2022-09-08 17:24:24 +0800

    delete mock data

--- src/services/comment.ts
+++ src/services/comment.ts
@@ -1,4 +1,3 @@
 import type { GetCommentApi } from '~/typings/comment'
 
-// export const getCommentApi: GetCommentApi = (episodeId, params) => axios.get(`/comment/${episodeId}`, { params })
-export const getCommentApi: GetCommentApi = (episodeId, params) => axios.get('/commentMock.json', { params, baseURL: '' })
+export const getCommentApi: GetCommentApi = (episodeId, params) => axios.get(`/comment/${episodeId}`, { params })
